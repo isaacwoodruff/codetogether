7521e7769be9985f6eb5d6d9eefcd73d0e12de59
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-09-28 12:36:33 +0000

    Added a base and index template, added index view

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'main_db'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
