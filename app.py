8f706fb103f04783178a9b707e692da833bc9bac
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-09-29 17:53:50 +0000

    Added update functionality, refactored edit_profile form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -14,15 +14,38 @@ mongo = PyMongo(app)
 def index():
     return render_template('index.html')
 
-@app.route('/edit-profile')
+@app.route('/edit_profile')
 def edit_profile():
-    user = mongo.db.users.find_one({u"_id": ObjectId('5d84afe31c9d440000f48258')})
-    return render_template('edit-profile.html', user=user)
+    user = mongo.db.users.find_one({"_id": ObjectId('5d90d9dd1c9d440000f6a0b6')})
+    return render_template('edit_profile.html', user=user)
     
 @app.route('/user/<user_id>')
 def user_profile(user_id):
-    user = mongo.db.users.find_one({u"_id": ObjectId(user_id)})
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
     return render_template('user_profile.html', user=user)
+    
+@app.route('/update_profile', methods=["POST"])
+def update_profile():
+    user = mongo.db.users
+    user.update( {'_id': ObjectId("5d90d9dd1c9d440000f6a0b6")},
+    {
+        '$set': {
+            'first_name':request.form.get('first_name'),
+            'last_name':request.form.get('last_name'),
+            'password': request.form.get('password'),
+            'description': request.form.get('description'),
+            'avatar': request.form.get('avatar'),
+            'about': request.form.get('about'),
+            'expertise': request.form.get('expertise').strip().split(","),
+            'looking_to.$[]': request.form.get('looking_to'),
+            'contact.email':request.form.get('email'),
+            'contact.skype':request.form.get('skype'),
+            'contact.github':request.form.get('github'),
+            'contact.linkedin':request.form.get('linkedin'),
+            'contact.discord':request.form.get('discord')
+        }
+    })
+    return redirect(url_for('edit_profile'))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
