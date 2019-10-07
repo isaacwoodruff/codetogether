8064b83befb4fb738af942f282c61f4f1ecbabe7
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-10-07 00:15:28 +0000

    Updated styling to messages

--- static/js/messages.js
+++ static/js/messages.js
@@ -1,23 +1,28 @@
-var socket = io.connect('https://' + document.domain + ':' + location.port);
+$(document).ready(function() {
+    var messageBox = document.getElementById("messages-wrapper");
+    
+    var socket = io.connect('https://' + document.domain + ':' + location.port);
 
-socket.on('connect', function() {
-    socket.emit('server message', {
-        data: 'User Connected'
-    })
-    $('form').on('submit', function(e) {
-        e.preventDefault()
+    socket.on('connect', function() {
         socket.emit('server message', {
-            user_name: $('input.hide').val(),
-            message: $('input.message').val()
+            data: 'User Connected'
         })
-        $('input.message').val('').focus()
+        $('form').on('submit', function(e) {
+            e.preventDefault()
+            socket.emit('server message', {
+                user_name: $('input.hide').val(),
+                message: $('input.message').val()
+            })
+            $('input.message').val('').focus()
+        })
     })
-})
 
-socket.on('client message', function(msg) {
-    console.log(msg)
-    if (typeof msg.user_name !== 'undefined') {
-        $('h3').remove()
-        $('div.message_holder').append('<div><h6>' + msg.user_name + ": " + msg.message + '</h6></div>')
-    }
+    socket.on('client message', function(msg) {
+        if (typeof msg.user_name !== 'undefined') {
+            $('h3').remove()
+            $('div.message_holder').append('<div><h6>' + msg.user_name + ": " + msg.message + '</h6></div>')
+            // Scroll to the bottom when a new message is received or sent
+            messageBox.scrollTop = messageBox.scrollHeight;
+        }
+    })
 })
